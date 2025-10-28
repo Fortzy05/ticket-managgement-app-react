12ca674e2e56eb2be863f4c944fca6edb531b678
Fortzy05 <omorodion.fortune@gmail.com>
2025-10-28 21:36:12 +0100

    feat: updated functionality of the ticket management

--- src/pages/ManageTicket.tsx
+++ src/pages/ManageTicket.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+
 import { useTickets } from "../context/TicketContext";
 import { useAuth } from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
